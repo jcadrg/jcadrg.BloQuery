d8323822dbfdc3b5093a667073fc3235ab89708e
Carlos Gomez <jcadrg@gmail.com>
2015-09-03 17:21:20 -0400

    Added Profile View Controllers

---
+++ jcadrg.BloQuery/jcadrg.BloQuery/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  jcadrg.BloQuery
+//
+//  Created by Mac on 9/3/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
---
+++ jcadrg.BloQuery/jcadrg.BloQuery/UserProfileViewController.m
@@ -0,0 +1,30 @@
+//
+//  UserProfileViewController.m
+//  jcadrg.BloQuery
+//
+//  Created by Mac on 9/3/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import "UserProfileViewController.h"
+#import "User.h"
+
+@interface  UserProfileViewController()
+
+@end
+
+
+@implementation UserProfileViewController
+
+-(void) viewDidLoad{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+-(void) didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
