f38da24d1ed9907dccca356b27092a96f15dd9a9
Carlos Gomez <jcadrg@gmail.com>
2015-09-04 02:31:55 -0400

    User profile won't show the picture nor the description of the user

---
+++ jcadrg.BloQuery/jcadrg.BloQuery/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  jcadrg.BloQuery
+//
+//  Created by Mac on 9/3/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
---
+++ jcadrg.BloQuery/jcadrg.BloQuery/TabBarController.m
@@ -0,0 +1,13 @@
+//
+//  TabBarController.m
+//  jcadrg.BloQuery
+//
+//  Created by Mac on 9/3/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@implementation TabBarController
+
+@end
